070cfac79da304250d4d65a4c0f6c055e3f544b8
kkstar <kouyatekarim02@gmail.com>
2020-11-03 18:49:52 +0000

    migrations and models

---
+++ app/Models/Question.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
+    public function tags()
+    {
+        return $this->hasMany('App\Models\Tag');
+    }
+}
---
+++ app/Models/Tag.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    public function questions()
+    {
+        return $this->hasMany('App\Models\Question');
+    }
+
+    
+}
