cdf3e3939ae2777917be31c16be3badb463dae82
juvenal Pengele <juvenalpengele@arolitec.com>
2020-12-14 17:37:23 +0000

    Integrate livewire registration component

---
+++ app/Http/Livewire/RegistrationForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RegistrationForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.registration-form');
+    }
+}
